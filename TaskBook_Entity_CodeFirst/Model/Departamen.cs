57b6dd36f9f09778967779a5b30e0904bb777cc4
Vinni949 <Pumba0@yandex.ru>
2021-05-23 10:13:28 +0300

    Add Departamen

---
+++ TaskBook_Entity_CodeFirst/Model/Departamen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskBook_Entity_CodeFirst.Model
+{
+    class Departamen
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public List<Consumer> Consumers { get; set; }
+        public List<Executor> Executors { get; set; }
+        public Departamen()
+        {
+            Executors = new List<Executor>();
+            Consumers = new List<Consumer>();
+        }
+        
+    }
+}
