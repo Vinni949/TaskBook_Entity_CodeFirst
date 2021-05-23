4bcf3538e1cfb10956bd63c82150150c4c4ac457
Vinni949 <Pumba0@yandex.ru>
2021-05-23 08:54:45 +0300

    Initialization

---
+++ TaskBook_Entity_CodeFirst/Model/TaskBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskBook_Entity_CodeFirst.Model
+{
+    class TaskBook
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string TechnicalSpecification { get; set; }
+        public int Priority { get; set; }
+        public int Deadline { get; set; }
+        public int Status_ID { get; set; }
+        public int Executor_ID { get; set; }
+        public int Consumer_ID { get; set; }
+
+    }
+}
---
+++ TaskBook_Entity_CodeFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskBook_Entity_CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
